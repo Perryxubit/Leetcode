cf53e9cbfa7158f3dfd7ce46d975acf6a1fc53d0
Perry Xu <xupubit@163.com>
2017-06-12 15:23:24 +0800

    migrate to .java file

--- Q001-Q050_Java/Q026_Remove_Duplicates_from_Sorted_Array.java
+++ Q001-Q050_Java/Q026_Remove_Duplicates_from_Sorted_Array.java
@@ -1,5 +1,5 @@
 /*
-    非常简单的题，对排序的数组去重复，并把不重复的数移动到前N个位置。
+    非常简单而又基础的题，对排序的数组去重复，并把不重复的数移动到前N个位置。
     具体实现见代码。
 
 */
@@ -8,7 +8,7 @@ public class Solution {
     public int removeDuplicates(int[] nums) {
         int j=1;
 		for(int i=1; i<nums.length; i++) {
-			if(nums[i] == nums[i-1]) { //pass
+			if(nums[i] == nums[i-1]) { //same value, pass!
 				continue;
 			}
 			else {
