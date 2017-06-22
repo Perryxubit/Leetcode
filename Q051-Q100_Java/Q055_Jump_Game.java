20d1d9d350a01e58b4d32d220c9edffa63be2c17
Perry Xu <xupubit@163.com>
2017-06-22 15:26:07 +0800

    initial check in

--- Q051-Q100_Java/Q055_Jump_Game.java
+++ Q051-Q100_Java/Q055_Jump_Game.java
@@ -1,9 +1,15 @@
 /*
-
+	Jump game, 给定数组{2, 3, 1, 1, 4}, 每一个元素是当前位置可以跳跃的最远距离。
+	第一种方法我用的深度优先搜索...递归的方式尝试找到最远的距离，见下文canJump函数，
+	但是跪在一个巨长的用例上{1,1,1,1,1,1,1,.......1}, 递归层数太多stack overflow了..
+	其实这个问题好像是贪心算法的经典问题? 我们定义最远距离变量arrive, 遍历数组中的每个元素，arrive取nums[i]+i和arive的较大值，
+	这样可以逐步选出最远可以跳到的距离，最后我们对比最远的距离变量arrive和nums中的最后一个元素的index就可以得出结果。
+	见canJump2函数。
 
 */
 
-public boolean canJump2(int[] nums) {
+public class Solution {
+	public boolean canJump2(int[] nums) {
 		//Greedy
 		int arrive = 0;
 		for(int i=0; i<nums.length; i++) {
@@ -29,3 +35,4 @@ public boolean canJump2(int[] nums) {
 		}
 		return false;
 	}
+}
